6a540f0abc9276f72c07786daed15d8938f8a245
OvidoBaia <ovidiobaia@gmail.com>
2021-06-07 18:12:52 -0300

    login com proprio componente

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <amplify-authenticator>
-      <Home v-if="authState === 'signedin' && user" :user="user" />
-    </amplify-authenticator>
-  </div>
+  <div class="container">
+    <!-- <amplify-authenticator>
+    </amplify-authenticator> -->
+    <div v-if="authState === 'signedin' && user"> 
+      <h1> teste</h1>
+      <Home :user="user" />
+    </div>
+    <form @submit.prevent="login">
+        <h2>Login</h2>
+        <input type="text" v-model="email" placeholder="Email address..." />
+        <input type="password" v-model="password" placeholder="password..." />
+        <button v-on:click="login">Login</button>
+    </form>
+    
+</div>
 </template>
 
 <script>
 import { onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { Auth } from 'aws-amplify';
 import Home from "./Home";
 
 export default {
@@ -23,6 +34,8 @@ export default {
       user: undefined,
       authState: undefined,
       unsubscribeAuth: undefined,
+      email:'',
+      password: ''
     };
   },
   beforeUnmount() {
@@ -31,6 +44,22 @@ export default {
   components: {
     Home,
   },
+  methods: {
+    async login() {
+    try {
+      //console.log(this.email)
+        const u = await Auth.signIn(this.email, this.password);
+        //this.unsubscribeAuth = u.onAuthUIStateChange
+        console.log(u.username)
+        this.user = {username:u.username}
+        this.authState = "signedin";
+        alert('Successfully logged in');
+    } catch (error) {
+        alert(error.message);
+    }
+},
+  }
+  
 };
 </script>
 
@@ -42,5 +71,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+amplify-authenticator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  height: 100vh;
 }
 </style>
